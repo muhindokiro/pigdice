ed650fba27a17f9811e79b2897098b954aa7a07c
muhindokiro <blaize1143@gmail.com>
2019-05-11 16:35:11 +0300

    add javascript style to show and hide text when about and rules buttons are click

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+$(".clickable").click(function(){
+  $(".about-hidden").toggle();
+  $(".about-showing").toggle();
+});
+$(".clickable1").click(function(){
+  $(".rules-hidden").toggle();
+  $(".rules-showing").toggle();
+});
+})
